9aa1ba560fde1f15cf51e8646ac22d74e8f52f66
Maxwell Silva <ctl_maxsilva@uolinc.com>
2023-11-30 13:22:49 -0400

    Teste de update

---
+++ app.py
@@ -0,0 +1,12 @@
+print("Testando")
+idade = 8
+
+class Pessoa:
+    def _init_(self, nome, idade):
+        self.name - nome
+        self.age = idade
+
+pessoa = Pessoa("Maxwell", 36)
+
+print(pessoa.name)
+print(pessoa.age)
